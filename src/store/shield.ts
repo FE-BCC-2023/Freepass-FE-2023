79cbb36301d2ba044476807559d789baf111e5c9
Daffa Izzuddin <rahmatullohdaffaizzuddin1@gmail.com>
2023-01-26 17:42:37 +0700

    prepare store

---
+++ src/store/shield.ts
@@ -0,0 +1,32 @@
+import { writable } from "svelte/store";
+
+export const ShieldStyle = [
+  'flat',
+  'flat-square',
+  'for-the-badge',
+  'plastic',
+  'social',
+]
+
+export interface DynamicShieldProps {
+  url: string;
+  query: string;
+  prefix?: string;
+  suffix?: string;
+}
+
+export interface ShiledProps {
+  label?: string;
+  message?: string;
+  color?: string;
+  logo?: string;
+  style?: string;
+  dynamic?: DynamicShieldProps;
+}
+
+export const shield = writable<ShiledProps>({
+  label: 'label',
+  message: 'message',
+})
+
+export const dynamic = writable(false);
