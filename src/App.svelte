42d10bb7eff4bace21a1f31e52310b7e2e252228
Daffa Izzuddin <rahmatullohdaffaizzuddin1@gmail.com>
2023-01-26 12:38:52 +0700

    setup svelte

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<div class="min-h-screen min-w-screen bg-black">
+  <p>Hello World</p>
+</div>
+
+<style lang="postcss">
+  div {
+    @apply flex items-center justify-center;
+    & p {
+      @apply text-white;
+    }
+  }
+</style>
