f7fc2c0425b1f053842a151e950c5953bc115b75
Tengku rafi <t.mrr39@gmail.com>
2023-01-30 11:34:18 +0700

    deleted:    my-app/src/assets/styles/loading.css 	modified:   my-app/src/components/Loading.js

--- my-app/src/components/Loading.js
+++ my-app/src/components/Loading.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import '../assets/styles/loading.css'
 
 export default function Loading () {
     return (
         <>
             <Container className="d-flex align-items-center justify-content-center" style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
-            <div class="banter-loader">
-                <div class="banter-loader__box"></div>
-                <div class="banter-loader__box"></div>
-                <div class="banter-loader__box"></div>
-                <div class="banter-loader__box"></div>
-                <div class="banter-loader__box"></div>
-                <div class="banter-loader__box"></div>
-                <div class="banter-loader__box"></div>
-                <div class="banter-loader__box"></div>
-                <div class="banter-loader__box"></div>
-            </div>
+                <div className="spinner-border text-primary" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
             </Container>
         </>
     );
